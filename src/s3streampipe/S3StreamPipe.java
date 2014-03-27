9ec44e5eb605fe70049a7b86e926c422f30b90af
fjiji <benjaminji@gmail.com>
2014-03-27 13:37:50 -0700

    wip: timed task completed

--- src/s3streampipe/S3StreamPipe.java
+++ src/s3streampipe/S3StreamPipe.java
@@ -4,21 +4,44 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Queue;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-public class S3StreamPipe {
+public class S3StreamPipe extends TimerTask {
 	
-	Queue<String> lineBuf = new ConcurrentLinkedQueue<String> ();
+	private final Queue<String> lineBuf = new ConcurrentLinkedQueue<String> ();
+	
+	public void addLine(String str) {
+		lineBuf.add(str);
+	}
 
-	final String bucketName;
+	private final String bucketName;
 	
 	S3StreamPipe(String bucketname) {
 		this.bucketName = bucketname;
 	}
 	
+	public void run() {
+		System.out.println(">>>test fengji!!!");
+		String str;
+		StringBuilder builder = new StringBuilder();
+		while ((str = lineBuf.poll()) != null) {
+			builder.append(str);
+			builder.append("\n");
+		}
+		if (builder.length() != 0) {
+			System.out.println("fengji test: " + builder.toString());
+		}
+		System.out.println("<<<test fengji!!!");
+	}
+	
 	public static void main(String[] args) {
 		String bucketName = args[0];
 		S3StreamPipe pipe = new S3StreamPipe(bucketName);
+		
+		Timer timer = new Timer();
+		timer.schedule(pipe, 2000, 2000); // 2 seconds delay and 2 second period
 		
 		/* the main loop to stream input into the lineBuf buffer */
 		while (true) {
@@ -29,8 +52,8 @@ public class S3StreamPipe {
 				while (true) {
 					String line = br.readLine();
 					if (line != null) {
-						System.out.println("fengji test: " + line);
-						pipe.lineBuf.add(line);
+						
+						pipe.addLine(line);
 					}
 				}
 				
